f360d4543d16bec503af0e5744d25c5ed58941c8
Mo Young <>
2020-07-09 23:16:33 +0800

    LeetCode 24: 两两交换链表中的节点

---
+++ Week_01/Day4/24_swap_nodes_in_pairs.py
@@ -0,0 +1,21 @@
+'''
+leetcode: 24
+'''
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def swapPairs(self, head: ListNode) -> ListNode:
+        thead = ListNode(-1)
+        thead.next = head
+        c = thead
+        while c.next and c.next.next:
+            a, b = c.next, c.next.next
+            c.next, a.next = b, b.next
+            b.next = a
+            c = c.next.next
+        return thead.next
