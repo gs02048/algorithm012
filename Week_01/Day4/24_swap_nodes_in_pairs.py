3f009449a33655549d800e8994b9595271dc5171
Mo Young <>
2020-07-11 23:23:43 +0800

    LeetCode 24: 两两交换链表中的节点

--- Week_01/Day4/24_swap_nodes_in_pairs.py
+++ Week_01/Day4/24_swap_nodes_in_pairs.py
@@ -10,12 +10,10 @@ leetcode: 24
 
 class Solution:
     def swapPairs(self, head: ListNode) -> ListNode:
-        thead = ListNode(-1)
-        thead.next = head
-        c = thead
-        while c.next and c.next.next:
-            a, b = c.next, c.next.next
-            c.next, a.next = b, b.next
-            b.next = a
-            c = c.next.next
-        return thead.next
+        pre, pre.next = self, head
+        while pre.next and pre.next.next:
+            a = pre.next
+            b = a.next
+            pre.next, b.next, a.next = b, a, b.next
+            pre = a
+        return self.next
